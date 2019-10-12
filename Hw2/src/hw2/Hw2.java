f5cf2293a2dbfed5c7deafa7ca24390aca168f92
Jakir Hossain Niloy <48614476+jakirvaw@users.noreply.github.com>
2019-10-12 10:47:44 +0600

    Add files via upload

---
+++ Hw2/src/hw2/Hw2.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Hw2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i,j,n;
+       Scanner rt=new Scanner(System.in);
+       n=rt.nextInt();
+       for(i=1;i<=n;i++)
+       {
+           
+           for(int o = 1; o <=n-i ; o++)
+           {
+               System.out.print(" ");
+           
+           }
+           for(j=i;j>=1;j--)
+           {
+               System.out.print(j);
+                     
+           }
+           for(int k=2;k<=i;k++)
+           {
+               System.out.print(k);
+               
+           }
+           System.out.println("");
+          
+          
+       }
+       //---------------------------------------------------
+            for(i=n;i>=1;i--)
+       {
+           
+           for(int o = 1; o <=n-i ; o++)
+           {
+               System.out.print(" ");
+           
+           }
+           for(j=i;j>=1;j--)
+           {
+               System.out.print(j);
+                     
+           }
+           for(int k=2;k<=i;k++)
+           {
+               System.out.print(k);
+               
+           }
+           System.out.println("");
+          
+          
+       }
+    }
+    
+}
